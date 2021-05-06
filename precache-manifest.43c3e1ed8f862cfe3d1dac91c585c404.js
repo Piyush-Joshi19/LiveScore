45b50b0b4b08ad60e130a4531f45bb98a18c3a1f
Piyush-Joshi19 <piyushjoshi1901@gmail.com>
2021-05-06 09:43:05 +0530

    Updates

---
+++ precache-manifest.43c3e1ed8f862cfe3d1dac91c585c404.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5156aef521c9281ac83ffa44d6f949",
+    "url": "/LiveScore/index.html"
+  },
+  {
+    "revision": "7ef69c77e332915b9162",
+    "url": "/LiveScore/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "85860b4e403a9776cce7",
+    "url": "/LiveScore/static/css/main.4285fb76.chunk.css"
+  },
+  {
+    "revision": "7ef69c77e332915b9162",
+    "url": "/LiveScore/static/js/2.3ecdd386.chunk.js"
+  },
+  {
+    "revision": "7c31314e008d1a59ddfae5300ece5508",
+    "url": "/LiveScore/static/js/2.3ecdd386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85860b4e403a9776cce7",
+    "url": "/LiveScore/static/js/main.c6717a3b.chunk.js"
+  },
+  {
+    "revision": "92e5cf359b2b700e09f8",
+    "url": "/LiveScore/static/js/runtime-main.ee84eec6.js"
+  }
+]);
